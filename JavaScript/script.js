b5c59913b1254f78ccfe5d980d83f12da0fabeee
Lazarokaua <lazarokaua4@gmail.com>
2023-12-12 21:29:52 -0300

    falta stacks responsivo

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,10 +1,10 @@
-// let nome = prompt('Qual é o seu nome: ');
+let nome = prompt('Qual é o seu nome: ');
 
-// let m = document.getElementById("mensagem");
+let m = document.getElementById("mensagem");
 
-// if (nome === null || nome === "") {
-//     m.textContent = "Bem vindo, visitante sem nome!";
+if (nome === null || nome === "") {
+    m.textContent = "Bem vindo, visitante sem nome!";
     
-// } else {
-//     m.textContent = "Bem vindo(a) " + nome + "!";
-// }
+} else {
+    m.textContent = "Bem vindo(a) " + nome + "!";
+}
